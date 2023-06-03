ca2657d4b0c29a18257bd42ac8012ced981bedd5
marcuszou <marcus.zou@gmail.com>
2023-06-03 23:30:29 +0200

    react-app-mosh

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,43 @@
+//import MouseEvent from "react";
+
+import { useState } from "react";
+// { items: [], heading: string }
+interface Props {
+  items: string[];
+  heading: string;
+  // (item: string) => void
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  // Event Handler
+  // const handleClick = (event: MouseEvent) => console.log(event);
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length == 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {setSelectedIndex(index);
+                            onSelectItem(item);}
+            }
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
